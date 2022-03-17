0382fa6fcb35430950f319bd8d921d77fdaa5b1b
Luis Enrique Miguel Villegas <99372513+migueluisV@users.noreply.github.com>
2022-03-16 22:43:15 -0700

    Add files via upload

---
+++ CSharp-Problema1.Grados-Radianes.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+//El siguiente programa corresponde al problema 1 del capítulo Trigonometría:
+//Pasar de grados a radianes, usando múltiplos de 10 grados, desde 0 grados
+//hasta 360 grados.
+
+//Miguel Villegas Luis Enrique; migueluisV
+//16 de marzo del 2022
+
+namespace Problema1.Grados_Radianes
+{
+    class Program
+    {
+        static void GradosRadianes()
+        {
+            char Op = 's';
+            int Grados;
+            double Radianes;
+
+            while (Op == 's')
+            {
+                Console.Clear();
+
+                Console.WriteLine("-PASAR GRADOS A RADIANES CON MÚLTIPLOS DE 10-\n");
+                try
+                {
+                    Console.Write("Escribe una cantidad de grados que un múltiplo de 10: "); Grados = Convert.ToInt32(Console.ReadLine());
+                    if ((Grados % 10) != 0)
+                    {
+                        Console.Write("El número ingresado no es un múltiplo de 10.");
+                        Console.ReadKey();
+                        GradosRadianes();
+                    }
+                    else
+                    {
+                        Radianes = Math.Round(Grados * (Math.PI / 180), 4);
+                        Console.WriteLine("\n{0}° es igual a {1}rad.", Grados.ToString(), Radianes.ToString());
+                    }
+                }
+                catch (FormatException)
+                {
+                    Console.Write("El valor ingresado no es aceptado.");
+                    Console.ReadKey();
+                    GradosRadianes();
+                }
+
+                Console.Write("\n¿Ingresar otra cantidad? [s/n]: "); Op = Convert.ToChar(Console.ReadLine().ToLower());
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Console.Title = "Grados a radianes";
+            GradosRadianes();
+        }
+    }
+}
